43d78f033c86249053e3d7330766e2ac90742125
Abdulshakur <emperor.cruzy@gmail.com>
2021-11-17 13:56:56 +0100

    menu-item updated with routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/pages/homepage/homepage.component'
+
+const HatPage = (props) => {
+  console.log(props);
+  return(
+  <div>
+    <p>Welcome to HatPage</p>
+    <Link to={`${props.match.url}/1`} >hate page 1</Link>
+    <Link to={`${props.match.url}/2`} >hate page 2</Link>
+    <Link to={`${props.match.url}/3`} >hate page 3</Link>
+  </div>
+)
+}
+
+const Course = () => (
+  <div>
+    <p>Welcome to Course</p>
+  </div>
+)
+
+const HatId = (props) => {
+  console.log(props);
+  return(
+    <div>
+    <p>Your hat id is: {props.match.params.hatid}</p>
+  </div>
+  )
+}
 
 function App() {
   return(
     <div>
-      <Homepage />
+        <Route exact path='/' component={Homepage} />
+        <Route path='/commit/forthe/hats' component={HatPage} />
+        <Route path='/commit/forthe/hats/:hatid' component={HatId} />
+        <Route path='/course' component={Course} />
     </div>
   )
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './menu-item.style.scss'
+import  {withRouter} from 'react-router-dom'
 
-const MenuItem = ({title, imageUrl, size}) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+  <div className={`${size} menu-item`} 
+   onClick={()=> history.push(`${match.url}${linkUrl}`)}
+  >
     <div 
-      style={{ backgroundImage: `url(${imageUrl})`}}
       className='background-image'
+      style={{ backgroundImage: `url(${imageUrl})`}}
     />
 
-  <div>
       <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
           <span className='subtitle'>SHOP NOW</span>
       </div>
-  </div>
 </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem)
