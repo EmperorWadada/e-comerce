1aa775a7be1951e52666db79e7fafac59bcc2365
Abdulshakur <emperor.cruzy@gmail.com>
2021-12-11 12:54:02 +0100

    logger corrected

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import logger from "redux-logger";
 const middleware = [];
 
 if (process.env.NODE_ENV === 'development') {
-    middleware.push(middleware)
+    middleware.push(logger)
 }
 
 // Before using persist store
